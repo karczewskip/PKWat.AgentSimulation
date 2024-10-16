80760699f11565d1f64d51774b0bdda49176a30f
PawelKarczewski <karczewskipawel93@gmail.com>
2024-10-16 23:25:25 +0200

    Added state to environment

--- PKWat.AgentSimulation.Core/ISimulationEnvironment.cs
+++ PKWat.AgentSimulation.Core/ISimulationEnvironment.cs
@@ -2,12 +2,23 @@
 {
     using PKWat.AgentSimulation.Core.Snapshots;
 
-    public interface ISimulationEnvironment : ISnapshotCreator
+    public interface ISimulationEnvironment<SIMULATION_STATE> : ISnapshotCreator
     {
+        void LoadState(SIMULATION_STATE state);
+        void UpdateState();
     }
 
-    public class DefaultSimulationEnvironment : ISimulationEnvironment
+    public abstract class DefaultSimulationEnvironment<SIMULATION_STATE> : ISimulationEnvironment<SIMULATION_STATE>
     {
+        private SIMULATION_STATE _state;
+
+        public void LoadState(SIMULATION_STATE state)
+        {
+            _state = state;
+        }
+
+        public abstract void UpdateState();
+
         public object CreateSnapshot()
         {
             return this;
