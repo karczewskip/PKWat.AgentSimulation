0cfd846d528f2469dc71ca1cb37fc687bcba4736
karczewskip <karczewskipawel93@gmail.com>
2025-01-12 14:35:46 +0100

    Remove multiple simulation projects and related files
    Removed entire projects and associated files for Ant Colony Optimization, Butterfly Effect, Game of Life, Prey vs Predator, and Airport simulations. This includes application setup, UI components, simulation logic, and various classes for handling simulation stages and events. Updated the solution file to remove references to these projects and their configurations.

--- Examples/PKWat.AgentSimulation.Examples.AntColonyOptimizationAlgorithm/Simulation/FoodSource.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PKWat.AgentSimulation.Examples.AntColonyOptimizationAlgorithm.Simulation;
-public class FoodSource
-{
-    public required ColonyCoordinates Coordinates { get; set; }
-    public required double SizeRadius { get; set; }
-}
