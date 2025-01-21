514be7c229b9919d86f915901fbf9494c5fbbc6d
karczewskip <karczewskipawel93@gmail.com>
2025-01-21 20:36:20 +0100

    Dodane benchmarki

---
+++ PKWat.AgentSimulation.Benchmark/Program.cs
@@ -0,0 +1,69 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using Microsoft.Extensions.DependencyInjection;
+using PKWat.AgentSimulation.Benchmark.SimulationExample;
+using PKWat.AgentSimulation.Core;
+
+public class AllowNonOptimized : ManualConfig
+{
+    public AllowNonOptimized()
+    {
+        // Zezwól na brak optymalizacji JIT
+        AddValidator(JitOptimizationsValidator.DontFailOnError);
+
+        // Dodaj domyślnych eksporterów
+        AddExporter(DefaultExporters.Csv);
+        AddExporter(DefaultExporters.Html);
+        AddExporter(DefaultExporters.Markdown);
+
+        // Dodaj domyślnych loggerów
+        AddLogger(ConsoleLogger.Default);
+
+        // Dodaj domyślnych dostawców kolumn
+        AddColumnProvider(DefaultColumnProviders.Instance);
+    }
+}
+
+public class Test
+{
+    private IServiceProvider serviceProvider;
+
+    public Test()
+    {
+        var services = new ServiceCollection();
+        services.AddAgentSimulation(typeof(SimulationRunner).Assembly);
+        services.AddScoped<SimulationRunner>();
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    [Benchmark]
+    public async Task Test1000()
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        await scope.ServiceProvider.GetRequiredService<SimulationRunner>().RunSimulation(1000);
+    }
+
+    [Benchmark]
+    public async Task Test2000()
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        await scope.ServiceProvider.GetRequiredService<SimulationRunner>().RunSimulation(2000);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new AllowNonOptimized();
+        var summary = BenchmarkRunner.Run<Test>(config);
+    }
+}
---
+++ PKWat.AgentSimulation.Benchmark/SimulationExample/SimulationRunner.cs
@@ -0,0 +1,46 @@
+﻿namespace PKWat.AgentSimulation.Benchmark.SimulationExample;
+
+using PKWat.AgentSimulation.Core;
+using PKWat.AgentSimulation.Core.Agent;
+using PKWat.AgentSimulation.Core.Builder;
+using PKWat.AgentSimulation.Core.Crash;
+using PKWat.AgentSimulation.Core.Environment;
+using PKWat.AgentSimulation.Core.Stage;
+using System.Linq;
+using System.Threading.Tasks;
+
+internal class SimulationRunner(ISimulationBuilder simulationBuilder)
+{
+    public async Task RunSimulation(int numberOfAgents)
+    {
+        var simulationContext = simulationBuilder.CreateNewSimulation<ExampleEnvironment>()
+            .AddAgents<AgentWithCounter>(numberOfAgents)
+            .AddStage<UpdateCountersStage>()
+            .AddCrashCondition(c => c.GetAgents<AgentWithCounter>().Any(x => x.Counter > 1000) ? SimulationCrashResult.Crash("Counter exceeded") : SimulationCrashResult.NoCrash)
+            .Build();
+
+        await simulationContext.StartAsync();
+    }
+
+    public class ExampleEnvironment : DefaultSimulationEnvironment
+    {
+    }
+
+    public class AgentWithCounter : SimpleSimulationAgent
+    {
+        public int Counter { get; set; }
+    }
+
+    public class UpdateCountersStage : ISimulationStage
+    {
+        public async Task Execute(ISimulationContext context)
+        {
+            var agents = context.GetAgents<AgentWithCounter>().ToArray();
+            foreach (var agent in agents)
+            {
+                agent.Counter++;
+            }
+        }
+    }
+
+}
